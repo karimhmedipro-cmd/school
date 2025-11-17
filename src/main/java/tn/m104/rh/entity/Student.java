0fb9bfc6d3c220b3e1dd27f48a1a9fb45ab7bdec
karimhmedipro-cmd <karimhmedipro@gmail.com>
2025-11-17 12:22:34 +0100

    first commit

---
+++ src/main/java/tn/m104/rh/entity/Student.java
@@ -0,0 +1,26 @@
+package tn.m104.rh.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+// premier test de commit .
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer rollNumber;
+    String name;
+    String address;
+    Double percentage;
+
+}
+// fin bloc entitt
